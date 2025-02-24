9c77f94e7adf664c044b4b5d1a1fc4ad92d1dbc0
SengQ1011 <sengjkb@gmail.com>
2025-02-25 00:09:41 +0800

    "set mouse inside window"

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,31 @@ int main(int, char**) {
 	App app;
 	app.SetCurrentScene(std::make_shared<MainMenuScene>());
 
-	bool isMaximized = false;
-	SDL_Event event;
-	if (event.type == SDL_WINDOWEVENT)
-	{
-		switch (event.window.event)
-		{
-		case SDL_WINDOWEVENT_MAXIMIZED:
-			isMaximized = true;
-			std::cout << "視窗已最大化" << std::endl;
-			break;
-
-		case SDL_WINDOWEVENT_RESTORED:
-			if (isMaximized) {
-				isMaximized = false;
-				std::cout << "視窗已從最大化還原" << std::endl;
-			}
-			break;
-
-		case SDL_WINDOWEVENT_SIZE_CHANGED:
-			std::cout << "視窗大小已變更為: "
-					  << event.window.data1 << " x " << event.window.data2
-					  << std::endl;
-			break;
-		}
-	}
+	// bool isMaximized = false;
+	// SDL_Event event;
+	// if (event.type == SDL_WINDOWEVENT)
+	// {
+	// 	switch (event.window.event)
+	// 	{
+	// 	case SDL_WINDOWEVENT_MAXIMIZED:
+	// 		isMaximized = true;
+	// 		std::cout << "視窗已最大化" << std::endl;
+	// 		break;
+	//
+	// 	case SDL_WINDOWEVENT_RESTORED:
+	// 		if (isMaximized) {
+	// 			isMaximized = false;
+	// 			std::cout << "視窗已從最大化還原" << std::endl;
+	// 		}
+	// 		break;
+	//
+	// 	case SDL_WINDOWEVENT_SIZE_CHANGED:
+	// 		std::cout << "視窗大小已變更為: "
+	// 				  << event.window.data1 << " x " << event.window.data2
+	// 				  << std::endl;
+	// 		break;
+	// 	}
+	// }
 
     while (!context->GetExit()) {
         switch (app.GetCurrentState()) {
