6a5619af5fe11def081b1b384d1e88f667757cbb
SengQ1011 <sengjkb@gmail.com>
2025-02-25 00:07:38 +0800

    "set mouse inside window"

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,39 @@
 #include "App.hpp"
 
 #include "Core/Context.hpp"
+#include <iostream>
 
 int main(int, char**) {
     auto context = Core::Context::GetInstance();
 	context->SetWindowIcon(RESOURCE_DIR "/pet00icon.png");
 	App app;
 	app.SetCurrentScene(std::make_shared<MainMenuScene>());
+
+	bool isMaximized = false;
+	SDL_Event event;
+	if (event.type == SDL_WINDOWEVENT)
+	{
+		switch (event.window.event)
+		{
+		case SDL_WINDOWEVENT_MAXIMIZED:
+			isMaximized = true;
+			std::cout << "視窗已最大化" << std::endl;
+			break;
+
+		case SDL_WINDOWEVENT_RESTORED:
+			if (isMaximized) {
+				isMaximized = false;
+				std::cout << "視窗已從最大化還原" << std::endl;
+			}
+			break;
+
+		case SDL_WINDOWEVENT_SIZE_CHANGED:
+			std::cout << "視窗大小已變更為: "
+					  << event.window.data1 << " x " << event.window.data2
+					  << std::endl;
+			break;
+		}
+	}
 
     while (!context->GetExit()) {
         switch (app.GetCurrentState()) {
